18ebdaae7a930eb1c33f97186e9b30ef744e7d5b
Maarten Pronk <git@evetion.nl>
2020-11-16 11:28:16 +0100

    Escapes user input in search_field to prevent XSS attacks.

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -15,9 +15,9 @@ def test_location() -> None:
 
 
 def test_location() -> None:
-    search_field = "8243LP"
+    search_field = "8243LP<>'"
     response = client.get("/by_location", params=dict(search_field=search_field))
     assert response.status_code == 200
     content = response.json()
     assert "location" in content
-    assert content.get("location", {}).get("search_field") == search_field
+    assert content.get("location", {}).get("search_field") == "8243LP&amp;lt;&amp;gt;'"
