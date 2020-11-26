d6c5ef5327e054cebb408ace0556491a1083dd73
Maarten Pronk <git@evetion.nl>
2020-11-26 11:28:53 +0100

    Fix only lat-lon input, added test for it.

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -5,7 +5,7 @@ from overstroomik_service.main import app
 client = TestClient(app)
 
 
-def test_location() -> None:
+def test_location_lon() -> None:
     longitude = 5.0
     response = client.get("/by_location", params=dict(longitude=longitude))
     assert response.status_code == 422
@@ -14,7 +14,19 @@ def test_location() -> None:
     assert content.get("location", {}).get("longitude") == longitude
 
 
-def test_location() -> None:
+def test_location_latlon() -> None:
+    longitude = 5.0
+    latitude = 53.0
+    response = client.get(
+        "/by_location", params=dict(longitude=longitude, latitude=latitude)
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert "location" in content
+    assert content.get("location", {}).get("longitude") == longitude
+
+
+def test_location_searchfield() -> None:
     search_field = "8243LP<>'"
     response = client.get("/by_location", params=dict(search_field=search_field))
     assert response.status_code == 200
